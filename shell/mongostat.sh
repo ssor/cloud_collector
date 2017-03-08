7e2dcef94037dd260db19204b9ec8fd857f0dd51
ssor <ssor@qq.com>
2017-03-08 17:55:24 +0800

    fix shell

--- shell/mongostat.sh
+++ shell/mongostat.sh
@@ -2,8 +2,8 @@
 
 result=""
 
-# for host in "172.16.1.11" "172.16.1.41" "172.16.1.42"
-for host in "127.0.0.1"
+# for host in "127.0.0.1"
+for host in "172.16.1.11" "172.16.1.41" "172.16.1.42"
 do 
     # echo $host
     conn=`mongostat --host=${host} --rowcount=1 --noheaders  | awk '{print $16}'`
